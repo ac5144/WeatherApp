91a0a91382e6c857198d0cf3b890d675931891ce
Andrew Chau <ac5144@nyu.edu>
2020-09-28 19:07:12 -0400

    Improve detailed view of weather

--- components/DayForecast.js
+++ components/DayForecast.js
@@ -12,20 +12,45 @@ const DayForecast = ({weatherData}) => {
 
 	const date = new Date(weatherData[0].dt * 1000);
 
-	const simpleView = <Text style={styles.forecastText} >{`${date.getMonth() + 1}/${date.getDate()}`}</Text>
-	const detailedView = weatherData.map((data, index) => <Text style={styles.forecastText} key={index}>{JSON.stringify(data)}</Text>)
-
 	const changeView = () => {
 
 		setDetailed(!detailed);
 	}
+	const getAverageTemp = () => {
+		if(!weatherData) {
+			return;
+		}
+
+		let sum = 0;
+
+		for (let i = 0; i < weatherData.length; i++) {
+			sum += weatherData[i].main.temp;
+		}
+
+		return sum / weatherData.length;
+	}
+
+	const detailedView = <View style={styles.detailedView}>
+							{weatherData.map((data, index) =>
+								<View key={index} style={styles.hourView}>
+									<Text style={styles.hourTextSmall}>{new Date(data.dt * 1000).getHours()}:00</Text>
+									<Text style={styles.hourTextSmall}>{((data.main.temp_min + data.main.temp_max) / 2).toFixed(0) + '\u00b0' + 'F'}</Text>
+									<Text style={styles.hourTextBig}>Humidity: {data.main.humidity}%</Text>
+									<Text style={styles.hourTextSmall}>{data.weather[0].main}</Text>
+								</View>
+							)}
+						 </View>
 
 	return (
 		<TouchableOpacity
 			style={styles.forecastContainer}
 			onPress={changeView}
 		>
-			{detailed ? detailedView : simpleView}
+			<View style={styles.simpleView}>
+				<Text style={styles.dateText}>{`${date.getMonth() + 1}/${date.getDate()}`}</Text>
+				<Text style={styles.tempText}>{`${getAverageTemp().toFixed(0) + '\u00b0'}F`}</Text>
+			</View>
+			{detailed && detailedView}
 		</TouchableOpacity>
 	);
 }
@@ -33,13 +58,36 @@ const DayForecast = ({weatherData}) => {
 const styles = StyleSheet.create({
 	forecastContainer: {
 		margin: 15,
-		padding: 10,
+		padding: 5,
 		borderStyle: 'solid',
 		borderBottomWidth: 1,
 		borderBottomColor: 'midnightblue'
 	},
-	forecastText: {
+	dateText: {
 		fontSize: 16
+	},
+	tempText: {
+		fontSize: 16,
+		fontWeight: 'bold'
+	},
+	simpleView: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	detailedView: {
+		marginTop: 10,
+		flex: 1
+	},
+	hourView: {
+		flexDirection: 'row'
+	},
+	hourTextSmall: {
+		flex: 1,
+		fontSize: 14
+	},
+	hourTextBig: {
+		flex: 2,
+		fontSize: 14
 	}
 });
 
